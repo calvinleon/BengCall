494975a5d90d815ae502eb41e5b85454b8f74128
Calvin Leonardo <calvin.leon19@gmail.com>
2020-05-07 15:39:54 +0700

    Rename Booking -> Home

---
+++ BengCall/Controllers/User/UserVC.swift
@@ -0,0 +1,15 @@
+//
+//  UserVC.swift
+//  BengCall
+//
+//  Created by Calvin Leonardo on 07/05/20.
+//  Copyright © 2020 Calvin Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class UserVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
