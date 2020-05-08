8afe9b45ae2a55a40b73f20f625909137538a209
Calvin Leonardo <calvin.leon19@gmail.com>
2020-05-08 08:39:43 +0700

    Fix Home Carousel

--- BengCall/Models/Authoshop.swift
+++ BengCall/Models/Authoshop.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Calvin Leonardo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AutoShop
+{
+    var autoShopName = ""
+    var autoShopAddress = ""
+    var autoShopImg: UIImage
+    
+    init(autoShopName: String, autoShopImg: UIImage, autoShopAddress: String)
+    {
+        self.autoShopName = autoShopName
+        self.autoShopAddress = autoShopAddress
+        self.autoShopImg = autoShopImg
+    }
+    
+    
+    static func fetchAutoShop() -> [AutoShop]
+    {
+        return [
+            AutoShop(autoShopName: "Bengkel AHASS XYZ", autoShopImg: UIImage(named: "bengkel.png")!, autoShopAddress: "Jl Maju Jaya"),
+            AutoShop(autoShopName: "Bengkel AHASS XYZ", autoShopImg: UIImage(named: "bengkel.png")!, autoShopAddress: "Jl Maju Jaya"),
+            AutoShop(autoShopName: "Bengkel AHASS XYZ", autoShopImg: UIImage(named: "bengkel.png")!, autoShopAddress: "Jl Maju Jaya"),
+            AutoShop(autoShopName: "Bengkel AHASS XYZ", autoShopImg: UIImage(named: "bengkel.png")!, autoShopAddress: "Jl Maju Jaya"),
+        ]
+    }
+}
+
