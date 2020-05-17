8769705d6ea3fffdf1dfc4c1a5c640a23635d785
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-05-17 21:06:42 +0700

    Updating MapKit on UserLocationView

--- BengCall/Controllers/User/UserSearchLocationVC.swift
+++ BengCall/Controllers/User/UserSearchLocationVC.swift
@@ -8,83 +8,51 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 
 class UserSearchLocationVC: UIViewController {
-
+    
     @IBOutlet weak var userMapView: MKMapView!
     
     let locationManager = CLLocationManager()
-    let regionInMeters: Double = 5000
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        checkLocationServices()
-    }
-    
-    func setupLocationManager() {
-        
         locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManagerSetup()
+        
     }
     
-    func centerViewOnUserLocation() {
-        if let location = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+    func locationManagerSetup() {
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+}
+
+//MARK: - LOCATION MANAGER
+extension UserSearchLocationVC: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
             userMapView.setRegion(region, animated: true)
         }
     }
     
-    func checkLocationServices() {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
-        if CLLocationManager.locationServicesEnabled() {
-            
-            setupLocationManager()
-            checkLocationAuthorization()
-        }
-        else {
-            //show alert to user to turn this on
-        }
     }
     
-    func checkLocationAuthorization() {
-        
-        switch CLLocationManager.authorizationStatus() {
-            
-        case .authorizedWhenInUse:
-            userMapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            //Show an alert to users that they're restricted to turn on the location
-            break
-        case .denied:
-            //Show alert instructing users to turn the Location Service ON
-            break
-        case .authorizedAlways:
-            break
-        @unknown default:
-            break
-        }
-    }
-    
-}
-
-extension UserSearchLocationVC: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //When the user's location updated
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        userMapView.setRegion(region, animated: true)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        //When users change their own permissions
-        checkLocationAuthorization()
-    }
 }
