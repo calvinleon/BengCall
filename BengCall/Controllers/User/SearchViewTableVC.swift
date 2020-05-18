75268e8c69d745183e6a3dce025096eac1a9a950
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-05-18 08:48:30 +0700

    Finished setting up SearchLocationTable for UserSearchLocation

--- BengCall/Controllers/User/SearchViewTableVC.swift
+++ BengCall/Controllers/User/SearchViewTableVC.swift
@@ -15,77 +15,79 @@ class SearchViewTableVC: UITableViewController {
     var userMapView: MKMapView? = nil
     
     var mapSearchHandlerDelegate: MapSearchHandler?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    // MARK: - TABLE VIEW DATA SOURCE
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return matchItems.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath)
+        
+        let selectedItem = matchItems[indexPath.row].placemark
+        
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchItems[indexPath.row].placemark
+        mapSearchHandlerDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    // MARK: - METHODS FOR ADDRESS
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+      // Put a space between "4" and "Melrose Place"
+      let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil ) ? " " : " "
+      // Put comma between street and city/state
+      let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.subAdministrativeArea != nil) ? ", " : " "
+      // Put a space between "Washington" and "DC"
+      let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+      let addressLine = String(format: "%@%@%@%@%@%@%@",
+                               // Street number
+                               selectedItem.subThoroughfare ?? " ",
+                               firstSpace,
+                               // Street name
+                               selectedItem.thoroughfare ?? " ",
+                               comma,
+                               // City
+                               selectedItem.locality ?? " ",
+                               secondSpace,
+                               // State
+                               selectedItem.administrativeArea ?? " "
+      )
+      return addressLine
     }
-    */
+}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    //MARK: - SEARCH CONTROLLER
+extension SearchViewTableVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let mapView = userMapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, _) in
+            guard let response = response else {
+                return
+            }
+            self.matchItems = response.mapItems
+            self.tableView.reloadData()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
