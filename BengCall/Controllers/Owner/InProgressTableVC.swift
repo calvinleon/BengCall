d72e1070e2a7f24058e9d52ddc97f3639fc20774
arifsetyoaji <arifsetyoaji18@gmail.com>
2020-05-20 00:20:39 +0700

    In progress and Profile view

--- BengCall/Controllers/Owner/InProgressTableVC.swift
+++ BengCall/Controllers/Owner/InProgressTableVC.swift
@@ -16,6 +16,8 @@ class InProgressTableVC: UITableViewController {
 //        navigationItem.largeTitleDisplayMode = .always
         
         super.viewDidLoad()
+        
+        self.tableView.separatorStyle = .none
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -26,25 +28,32 @@ class InProgressTableVC: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
+    
 
-    /*
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProgressCell", for: indexPath)
 
         // Configure the cell...
 
+        cell.selectionStyle = .none
+        
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
