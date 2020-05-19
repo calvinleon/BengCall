75cc75180792505377184117b3badcda47353226
arifsetyoaji <arifsetyoaji18@gmail.com>
2020-05-19 22:51:14 +0700

    setting tab bar nav for owner

---
+++ BengCall/Controllers/Owner/InProgressCell.swift
@@ -0,0 +1,24 @@
+//
+//  InProgressCell.swift
+//  BengCall
+//
+//  Created by Muhammad Arif Setyo Aji on 19/05/20.
+//  Copyright © 2020 Calvin Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class InProgressCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
