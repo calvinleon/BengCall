4f5dcd512939be2a352920c56e9528b296f094ed
Calvin Leonardo <calvin.leon19@gmail.com>
2020-05-26 16:41:02 +0700

    Add Image

--- BengCall/Controllers/User/AutoShopDetailVC.swift
+++ BengCall/Controllers/User/AutoShopDetailVC.swift
@@ -25,11 +25,7 @@ class AutoShopDetailVC: UIViewController {
     var autoshopImg = UIImage()
 
     override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.isNavigationBarHidden = false
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-        self.navigationController?.view.backgroundColor = UIColor.clear
+        
     }
     
     
