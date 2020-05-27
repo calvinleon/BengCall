53c7afb8d5ac086ff7647489a0100136cf09d068
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2020-05-27 09:59:04 +0700

    Adding UserHistory Current booking cell

--- BengCall/Controllers/User/UserHistoryVC.swift
+++ BengCall/Controllers/User/UserHistoryVC.swift
@@ -21,35 +21,26 @@ class UserHistoryVC: UIViewController, UITableViewDelegate {
         
         historyTableView.delegate = self
         
-        helper.fetchAll { (records) in
-            self.histories = records
-            DispatchQueue.main.async {
-                self.historyTableView.reloadData()
-            }
-        }
+//        helper.fetchAll { (records) in
+//            self.histories = records
+//            DispatchQueue.main.async {
+//                self.historyTableView.reloadData()
+//            }
+//        }
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension UserHistoryVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return histories.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return 0
+        let cell = tableView.dequeueReusableCell(withIdentifier: "completedBookings", for: indexPath)
+        
+        cell.textLabel?.text = histories[indexPath.row]["name"] as? String
+        
+        return cell
     }
 }
